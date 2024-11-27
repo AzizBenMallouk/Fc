956d32415920cc6b020f2483afa1bc3970aa4530
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-11-27 17:29:59 +0100

    feat : add details player with the i mark
    fix : position decalage

--- Js/builder.js
+++ Js/builder.js
@@ -26,23 +26,33 @@ const formations = [
         { pn : 11 ,x: 530, y: 395 , post: 'ST' ,player : -1},
     ]
 ]
-const playerUsed = []
+const playerUsed = [];
+let formationPicked = 0;
 const DefaultPositions = () =>{
-    const formationPicked = localStorage.getItem("fp") ?? 0 ;
+   formationPicked = localStorage.getItem("fp") ?? 0 ;
     formations[formationPicked].forEach((item, index) => {
         const element = document.createElement('div');
         element.style.transition = "all .5s,transform 1s"
         element.style.transform = "rotateY(0deg)"
         element.classList.add("absolute")
+        element.addEventListener("mouseover",()=>{
+            element.style.filter = 'drop-shadow(1px 1px 10px #c4c4c46e)';
+        })
+        element.addEventListener("mouseleave",()=>{
+            element.style.filter = 'none';
+            
+        })
         element.style.left = `${item.y}px`
         element.style.bottom = `${item.x}px`
         element.setAttribute("id" ,`img${index + 1}` )
-        element.setAttribute("onclick" ,`PickPlayer(${formationPicked},${item.pn},"${item.post}")` )
         element.classList.add('element');
         element.innerHTML = `<img id="imgcover${item.pn}" src="./Assets/image.png"  class="h-40 " alt="">
-                    ${item.player==-1 && 
+                
+                  
+           
+                ${item.player == -1 && 
                     (`
-                        <div id="addplayericon${item.pn}" class="absolute inset-0 flex justify-center items-center">
+                        <div onclick="PickPlayer(${item.pn})" id="addplayericon${item.pn}" class="absolute inset-0 flex justify-center items-center">
                             <div class="relative text-gray-400 text-2xl cursor-pointer">
                                 <i class="fa-solid fa-plus"></i>
                             </div>    
@@ -58,9 +68,16 @@ const DefaultPositions = () =>{
                         <img id="imgflag${item.pn}" class="w-5 "  />
                     </div>
                     <img id="imgplayer${item.pn}" class="absolute left-10 w-16 top-6"  />
-                    <div class="absolute left-8 right-8 top-24 font-bold text-xs flex justify-center items-center text-center">
-                        <h2 id="playername${item.pn}" class=""></h2>
+                    <div class="absolute left-4 right-4 top-[88px]  text-[0.6rem] flex flex-col justify-center items-center text-center">
+                        <h2 class="font-bold" id="playername${item.pn}" class=""></h2>
+                        
+                        <div class="flex flex-col items-center">
+                            <span id="playerclub${item.pn}" class="text-[0.5rem]"></span>
+                            <img id="imgclub${item.pn}" class="w-4"  />
+                        </div>
+                        
                     </div>
+                    
         ` ;
         canvas.appendChild(element);
 
@@ -79,6 +96,12 @@ for (let index = 0; index <= 11 ; index++) {
 function onchangeformation(){
     const tt = document.getElementById("formationtaktik").value;
     tt && localStorage.setItem("fp",tt) 
+    tt && (formationPicked = Number(tt));
+    formations.forEach((taktik,i)=>{
+        i!=formationPicked && taktik.forEach((item,p)=>{
+            item.player = formations[formationPicked][p].player;
+        })
+    })
 
     for (let i = 0; i < players.length; i++) {
         players[i].style.left =  `${formations[Number(tt)][i].y }px`;
@@ -86,6 +109,7 @@ function onchangeformation(){
         const gg = document.getElementById(`post${i}`);
         gg && (gg.innerHTML = formations[Number(tt)][i].post );
     }
+    console.log(formations)
 }
 function OpenFormationMenu(){
     const formations = document.getElementById('formations')
@@ -103,50 +127,52 @@ function OpenFormationMenu(){
         formationcontent.innerHTML = `<select name="" id="formationtaktik" 
                     class="mt-4 py-2  w-full text-gray-400 bg-transparent rounded-md border-2 border-gray-600"
                     onchange="onchangeformation()">
-                        <option value="0">4-4-2</option>
-                        <option value="1">4-3-3</option>
-                        <option value="1">3-5-2</option>
-                        <option value="1">3-4-3</option>
-                        <option value="1">5-3-1</option>
+                        <option value="0" >4-4-2</option>
+                        <option value="1" >4-3-3</option>
+                        
                     </select>`;
-        formations.setAttribute("ipOpened",'true')
+        formations.setAttribute("ipOpened",'true');
+
+        document.getElementById("formationtaktik").selectedIndex = formationPicked;
     }
 }
 
-function PickPlayer(formationPicked , pn , postVar){
+function PickPlayer(pn ){
     
     fetch('../Data/players.json')
     .then(res=>res.json())
     .then(data=> {
-        console.log(postVar)
-        AddPlaperPanel(data ,formationPicked , pn , postVar)
+        // console.log(Array.from(formations[formationPicked][pn-1].post).slice(-2).join())
+        AddPlaperPanel(data ,
+             pn ,
+             `${Array.from(formations[formationPicked][pn-1].post).slice(-2).join().replace(/,/g,"")}`)
     })
 }
 function ClosePanlePlayers(){
     document.getElementById("playerspanel").innerHTML = ``;
 }
-function AddPlaperPanel(data ,formationPicked , pn , post){
-    console.log(post);
+function AddPlaperPanel(data  , pn , post){
     let PlayerCards = ``;
-
+    console.log()
     data && data.forEach((item , i)=>{
         // const newCard = document.createElement('div');
         let FindPlayer= data.find((row)=>row.id==item.id);
         item.position.includes(""+post) ? PlayerCards += `
-        <div class='relative cursor-pointer' onclick='PickedPlayer(${JSON.stringify(FindPlayer).replace(/'/g, "")},${formationPicked},${pn},${item.id})'>
-        <img src="./Assets/badge-white.png"  class="h-44 " alt="">
-       
-        <div class="absolute left-[18px] top-8 flex flex-col items-center">
-            <h2 class="m-0 p-0 font-bold text-ms">${item.rating}</h2>
-            <span class="text-[8px]">${item.position}</span>
-            <img class="w-5 " src="${item.flag}"   />
+        <div class='relative shadow-md cursor-pointer' onclick='PickedPlayer(${JSON.stringify(FindPlayer).replace(/'/g, "")},${pn},${item.id})'>
+            <img src="${item.cover ?? './Assets/badge-white.png'}" 
+            class="${item.rating>85 ?'w-28' : 'w-28'}" alt="">
+            
+            <div class="absolute left-[16px] top-[30px] flex flex-col items-center">
+                <h2 class="m-0 p-0 font-bold text-ms ${item.rating>85 && (item.position != "GK" &&'text-[#FFD972]')}">${item.rating}</h2>
+                <span class="text-[8px] font-bold ${item.rating>85 && (item.position != "GK" &&'text-[#FFD972]')}">${item.position}</span>
+                <img class="w-5 ${item.rating>85 &&(item.position != "GK" &&'text-[#FFD972]')}" src="${item.flag}"   />
+            </div>
+            <img class="absolute left-10 w-16 top-6" src="${item.photo}" />
+            <div class="absolute left-8 right-8 top-24 font-bold text-xs flex justify-center items-center text-center">
+                <h2 class="${item.rating>85 && (item.position != "GK" &&'text-[#FFD972]')} ">${item.name}</h2>
+            </div>
+            
         </div>
-        <img class="absolute left-10 w-16 top-6" src="${item.photo}" />
-        <div class="absolute left-8 right-8 top-24 font-bold text-xs flex justify-center items-center text-center">
-            <h2 class="">${item.name}</h2>
-        </div>
-        </div>
-        
         ` : ''
     })
 
@@ -165,24 +191,92 @@ function AddPlaperPanel(data ,formationPicked , pn , post){
     `;
 }
 
-function PickedPlayer(playerObject , formationPicked , pn ,id){
+function PickedPlayer(playerObject , pn ,id){
     formations[formationPicked].forEach((itm ,i)=>{
         if(itm.pn==pn) {
             itm.player = id ;
         }
     })
     const Card = document.getElementById(`img${pn}`);
+    Card.innerHTML += 
+    `
+        <div onclick='getPlayerInfo(${JSON.stringify(playerObject)})' class="absolute z-20 cursor-pointer right-0 bottom-10 bg-gray-600 text-white rounded-full h-5 w-5 flex justify-center items-center shadow-sm">
+            <span class="font-sans">i</span>
+        </div>
+    `;
     document.getElementById(`ratingtext${pn}`).innerHTML = playerObject?.rating;
     document.getElementById(`playername${pn}`).innerHTML = playerObject?.name;
     document.getElementById(`posttext${pn}`).innerHTML = playerObject?.position;
+    document.getElementById(`playerclub${pn}`).innerHTML = playerObject?.club;
+
+    (playerObject?.rating >85 && playerObject?.position != "GK" ) && 
+    ( document.getElementById(`ratingtext${pn}`).classList.add('text-[#FFD972]'),
+    document.getElementById(`playername${pn}`).classList.add('text-[#FFD972]'),
+    document.getElementById(`posttext${pn}`).classList.add('text-[#FFD972]'),
+    document.getElementById(`playerclub${pn}`).classList.add('text-[#FFD972]')
+    )
+    
+
     const addplayericon = document.getElementById(`addplayericon${pn}`);
     addplayericon.style.display = 'none';
-    const allimgs = document.querySelectorAll(`#img${pn} img`);
     document.getElementById(`imgcover${pn}`).setAttribute("src" , playerObject?.cover )
     document.getElementById(`imgflag${pn}`).setAttribute("src" , playerObject?.flag )
     document.getElementById(`imgplayer${pn}`).setAttribute("src" , playerObject?.photo )
+    document.getElementById(`imgclub${pn}`).setAttribute("src" , playerObject?.logo )
+
     Card.style.transform = "rotateY(0deg) scale(1.1)"
     setTimeout(()=>{
         Card.style.transform = "rotateY(0deg) scale(1)"
     },500)
+}
+
+function getPlayerInfo(playerObject){
+    document.getElementById(`playerinfo`).innerHTML = 
+    `
+        <div class="border-[#3C4053] rounded-md border-2 w-full py-5 px-6">
+            <div class="flex justify-between">
+            <h3 class=" text-gray-100">Player information</h3>
+                <i class="fa-solid fa-xmark text-white cursor-pointer" onclick="ClosePanlePlayersinfo()"></i>
+            </div>
+            <div class="text-white text-xs grid grid-cols-[1fr,auto] pt-4">
+                <div class="flex flex-col gap-4 w-full ">
+                    <div class="flex flex-col items-center">
+                        <div class="flex gap-3">
+                            <img class="w-16" src="${playerObject?.flag}" >
+                        </div>
+                        <h3 class="font-semibold">${playerObject?.nationality}</h3>
+                    </div>
+                    <div class="grid grid-cols-2 justify-between">
+                        <div>
+                            <p>position : ${playerObject?.position}</p>
+                            <p>club :${playerObject?.club}</p>
+                            <p>rating :${playerObject?.rating}</p>
+                            <p>pace : ${playerObject?.pace}</p>
+                            <p>shooting : ${playerObject?.shooting}</p>
+                        </div>
+                        <div>
+                            <p>passing : ${playerObject?.passing}</p>
+                            <p>dribbling : ${playerObject?.dribbling}</p>
+                            <p>defending : ${playerObject?.defending}</p>
+                            <p>physical : ${playerObject?.physical}</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="grid gap-1">
+                     <img class="w-28 border-2 border-gray-300 bg-[#3a357357] rounded-md" src="${playerObject?.photo}">
+                     <h2 class="font-bold">${playerObject?.name}</h2>
+                     <img class="w-10" src="${playerObject?.logo}" >
+                </div>
+                
+
+            </div>
+            
+
+        </div>
+    `;
+    
+}
+function ClosePanlePlayersinfo(){
+    document.getElementById(`playerinfo`).innerHTML= ``;
 }
